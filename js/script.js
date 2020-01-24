3e8f85b4024981c7a5b4362a4d707e82a17433f4
jspark721 <jspark721@gmail.com>
2020-01-23 17:01:26 -0800

    adding javascript animations

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let animateDescBox = function() {
+    let box = document.getElementsByClassName('desc');
+
+    let revealFirstBox = function() {
+        box[0].style.opacity = 1;
+        box[0].style.transform = "scaleX(1) translateY(0)";
+    }
+}
