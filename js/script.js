083bdafea786163134241e6079f3df46ac779dcd
jspark721 <jspark721@gmail.com>
2020-01-24 15:21:40 -0800

    add animation to homepage

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-let animateDescBox = function() {
-    let box = document.getElementsByClassName('desc');
+let boxArray = document.getElementsByClassName('desc');
 
-    let revealFirstBox = function() {
-        box[0].style.opacity = 1;
-        box[0].style.transform = "scaleX(1) translateY(0)";
+let animateDescBox = function(box) {
+
+    let revealBox = function(i) {
+        boxArray[i].style.opacity = 1;
+        boxArray[i].style.transform = "scaleX(1) translateY(0)";
+        boxArray[i].style.msTransform = "scaleX(1) translateY(0)";
+        boxArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+
+    for(let i = 0; i < boxArray.length; i++) {
+        revealBox(i);
     }
+};
+
+window.onload = function() {
+    if(window.innerHeight > 950) {
+        animateDescBox(boxArray);
+    }
+
+    let desc = document.getElementsByClassName('desc')[0];
+    let scrollDistance = desc.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animateDescBox(boxArray);
+        }
+    })
 }
+
